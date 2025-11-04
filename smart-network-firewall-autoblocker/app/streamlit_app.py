fd14eb0b5553932def011daea391cadc034fc668
Manaswee Nadgouda <manaswee.nadgouda22@gmail.com>
2025-11-04 15:33:45 -0700

    Update streamlit_app.py

--- smart-network-firewall-autoblocker/app/streamlit_app.py
+++ smart-network-firewall-autoblocker/app/streamlit_app.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from datetime import datetime, timedelta
 
-from .detector import detect_events, DetectionConfig
-from .utils import load_firewall_log, load_conn_log, write_block, read_blocklist, ensure_files
+from app.detector import detect_events, DetectionConfig
+from app.utils import load_firewall_log, load_conn_log, write_block, read_blocklist, ensure_files
 
 
 st.set_page_config(page_title="Smart Network Firewall Auto-Blocker", layout="wide")
